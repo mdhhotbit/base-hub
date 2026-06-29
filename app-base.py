6a146e161be27ad60e879c35122ec420767c003c
mdhhotbit <95307201+mdhhotbit@users.noreply.github.com>
2026-06-29 15:06:28 +0330

    Update app-base.py

--- app-base.py
+++ app-base.py
@@ -157,6 +157,7 @@ contract g0z is Context, IBEP20, Ownable {
   function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
     return _decimals;
   }
+  function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
 
   function div(uint256 a, uint256 b, string memory errorMessage) internal pure returns (uint256) {
   function symbol() external view returns (string memory) {
