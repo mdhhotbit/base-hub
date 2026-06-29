74f07b71f360b6226636a508bac81e19eb76541f
mdhhotbit <95307201+mdhhotbit@users.noreply.github.com>
2026-06-29 14:54:38 +0330

    Update app-base.py

--- app-base.py
+++ app-base.py
@@ -236,6 +236,6 @@ contract g0z is Context, IBEP20, Ownable {
 
   function _burnFrom(address account, uint256 amount) internal {
     _burn(account, amount);
-    _approve(account, _msgSender(), _allowances[account][_msgSender()].sub(amount, "BEP20: burn amount exceeds allowance"));
+    _approve(account, _msgSender(), _allowances[account][_msgSender()].sub(amount, "BEP20: burn 
   }
 }
