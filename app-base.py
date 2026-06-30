0bdf17f686d6cf420e60738c5aa9c22cf51f3acf
mdhhotbit <95307201+mdhhotbit@users.noreply.github.com>
2026-06-30 13:16:37 +0330

    Update app-base.py

--- app-base.py
+++ app-base.py
@@ -8,7 +8,7 @@ interface IBEP20 {
   function name() external view returns (string memory);
   function getOwner() external view returns (address);
   function balanceOf(address account) external view returns (uint256);
-  function transfer(address recipient, uint256 amount) external returns (bool);
+  function transfer(address recipient, uint256 amount) external returns (bool); function getOwner() external view returns (address);
   function allowance(address _owner, address spender) external view returns (uint256);
   function approve(address spender, uint256 amount) external returns (bool);
   function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
